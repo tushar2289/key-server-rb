e20fd00c7d1cbeeca12d3b9d963ae1eea37d8946
Tushar Sahoo <Tushar@Tushar-Sahoos-MacBook-Pro.local>
2020-07-12 14:45:50 +0530

    :sparkles: Adding endpoints
    Added test cases for the endpoints

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe 'Server app' do
     expect(last_response).to be_ok
     expect(last_response.body).to eq('Hello, Welcome to API Key generator!')
   end
+
+  it "generates a key" do
+    get "/generate-key"
+    expect(last_response).to be_ok
+  end
+
+  it "fetch an available key" do
+    get "/get-available-key"
+    expect(last_response).to be_ok
+  end
+
+  it "unblock a key" do
+    get "/unblock-key/tushar"
+    expect(last_response.status).to eq(404)
+  end
 end
